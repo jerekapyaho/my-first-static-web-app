682a756574a3071903304e395ef335ec69f85454
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2021-11-04 08:52:49 +0200

    Tweaked C# function

--- DotNetMessage/DotNetMessage.cs
+++ DotNetMessage/DotNetMessage.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace api
+namespace DotNetMessage
 {
     public static class DotNetMessage
     {
